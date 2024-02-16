363a49980dea27f31964ba6d595f34d4be2fb2f9
TedPasquiet <tpasquiet@hotmail.fr>
2024-02-16 16:09:01 +0100

    Incorporer style et assets

---
+++ reactintro/src/components/App.js
@@ -0,0 +1,15 @@
+import Banner from "./Banner";
+import Cart from "./Cart";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Banner />
+        <Cart />
+      </div>
+    </>
+  );
+}
+
+export default App;
