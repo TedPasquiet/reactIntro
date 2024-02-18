bbfa2800036040aa124f1613deb1aa55a4658ee7
TedPasquiet <tpasquiet@hotmail.fr>
2024-02-18 22:34:11 +0100

    useState

--- reactintro/src/components/App.js
+++ reactintro/src/components/App.js
@@ -3,6 +3,7 @@ import logo from "../assets/leaf+1.png";
 import Footer from "./Footer";
 // import Cart from './Cart'
 import ShoppingList from "./ShoppingList";
+import Cart from "./Cart";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <img src={logo} alt="La maison jungle" className="lmj-logo" />
         <h1 className="lmj-title">La maison jungle</h1>
       </Banner>
+      <Cart />
       {/* <Cart /> */}
       <ShoppingList />
       <Footer />
--- reactintro/src/components/Cart.js
+++ reactintro/src/components/Cart.js
@@ -1,19 +1,28 @@
+import { useState } from "react";
 import "../styles/cart.css";
 
 function Cart() {
   const monsteraPrice = 8;
   const ivyPrice = 10;
   const flowerPrice = 15;
-  return (
+  // Création state cart avec valeur initiale à 0
+  // useState hook qui permet d'ajouter le state local react à des composants fonctions
+  // Décomposition du state du cart, en premier la valeur, en second la fonction qui permet de modifier
+  const [cart, updateCart] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+  return isOpen ? (
     <div className="lmj-cart">
+      <button onClick={() => setIsOpen(false)}>Fermer</button>
+      <button onClick={() => updateCart(0)}>Vider</button>
       <h2>Panier</h2>
-      <ul>
-        <li>Monstera : {monsteraPrice}€</li>
-        <li>Lierre : {ivyPrice}€</li>
-        <li>Fleurs : {flowerPrice}€</li>
-      </ul>
-      Total : {monsteraPrice + ivyPrice + flowerPrice}€
+      <div>
+        Monstera : {monsteraPrice}€
+        <button onClick={() => updateCart(cart + 1)}>Ajouter</button>
+      </div>
+      <h3>Total : {monsteraPrice * cart}€</h3>
     </div>
+  ) : (
+    <button onClick={() => setIsOpen(true)}>Ouvrir le Panier</button>
   );
 }
 
