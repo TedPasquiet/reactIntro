f9b66a61a7a84e6c679d76e54f4fe573839e05a5
TedPasquiet <tpasquiet@hotmail.fr>
2024-02-19 18:02:39 +0100

    Remonter state

--- reactintro/src/components/App.js
+++ reactintro/src/components/App.js
@@ -4,19 +4,22 @@ import Footer from "./Footer";
 // import Cart from './Cart'
 import ShoppingList from "./ShoppingList";
 import Cart from "./Cart";
+import { useState } from "react";
 
 function App() {
+  const [cart, updateCart] = useState([])
   return (
     <div>
-      <Banner>
-        <img src={logo} alt="La maison jungle" className="lmj-logo" />
-        <h1 className="lmj-title">La maison jungle</h1>
-      </Banner>
-      <Cart />
-      {/* <Cart /> */}
-      <ShoppingList />
-      <Footer />
+    <Banner>
+        <img src={logo} alt='La maison jungle' className='lmj-logo' />
+        <h1 className='lmj-title'>La maison jungle</h1>
+    </Banner>
+    <div className='lmj-layout-inner'>
+        <Cart cart={cart} updateCart={updateCart} />
+        <ShoppingList cart={cart} updateCart={updateCart} />
     </div>
+    <Footer />
+</div>
   );
 }
 
--- reactintro/src/components/ShoppingList.js
+++ reactintro/src/components/ShoppingList.js
@@ -2,7 +2,7 @@ import { plantList } from "../data/plantList";
 import PlantItem from "./PlantItem";
 import "../styles/ShoppingList.css";
 
-function ShoppingList() {
+function ShoppingList({cart,updateCart}) {
   const categories = plantList.reduce(
     (acc, plant) =>
       acc.includes(plant.category) ? acc : acc.concat(plant.category),
@@ -18,6 +18,7 @@ function ShoppingList() {
       </ul>
       <ul className="lmj-plant-list">
         {plantList.map(({ id, cover, name, water, light }) => (
+          <div>
           <PlantItem
             key={id}
             cover={cover}
@@ -25,6 +26,9 @@ function ShoppingList() {
             water={water}
             light={light}
           />
+          <button onClick={() => updateCart(cart + 1)}>Ajouter</button>
+          </div>
+
         ))}
       </ul>
     </div>
